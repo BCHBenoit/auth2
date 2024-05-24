311e1bc0ce1420ab6953718481413ee942673417
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-24 20:50:48 +0000

    guardado3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,38 @@ class SessionsController < ApplicationController
 
   def create
     # authenticate the user
-    # 1. try to find the user by their unique identifier
-    # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    # 1. try to find the user by their unique identifier (email)
+    @user_entered_email = params["email"]
+    @user_entered_password = params["password"]
+
+    @user_table = User.all
+    @user_row = @user_table.find_by("email"=> @user_entered_email)
+
+    if @user_row != nil
+      @user_email = @user_row["email"]
+      @user_password = @user_row["password"]
+
+      if BCrypt::Password.new(@user_password) == @user_entered_password
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      else  
+        redirect_to "/login"
+      end  
+    
+    else
+      redirect_to "/login"
+    end
+    #si es que el email que ingreso el usuario no estaba en la tabla va a traer NIL, porque
+    #no encontro coincidencia, caso contrario va traer el email del usuario. Lo mismo para el
+    #password
+
+
+    # 2. if the user exists -> check if they know their password, en otras palabras si ingresaron
+    #el password correcto de su cuenta.
+    
+
+
+    
   end
 
   def destroy
