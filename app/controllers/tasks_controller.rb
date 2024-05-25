eb8a584b1a15b3a732088f8d83c506201d6b6330
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-25 16:46:10 +0000

    guardado8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,17 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+
+    @users_table = User.all
+    @current_user_row = @users_table.find_by("id"=>session["user_id"])
+
+    @current_user_tasks = @tasks.where({"user_id"=>@current_user_row["id"]})
   end
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @task["user_id"] =session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
